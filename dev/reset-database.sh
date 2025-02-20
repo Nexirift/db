36ea080b0ad96cc7e8de61932f2c0f2be27421e1
Creaous <68230846+Creaous@users.noreply.github.com>
2025-02-21 00:17:50 +1030

    change development scripts

--- dev/reset-database.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f migrations/\*.sql
-rm -rf migrations/meta
-docker exec -ti nexirift-postgres dropdb -U postgres -f 'nexirift'
-docker exec -ti nexirift-postgres createdb -U postgres 'nexirift'
