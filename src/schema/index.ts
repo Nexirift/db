b9aeebb450617ab9e0cf6b3fc6215cdcd36396c9
Creaous <68230846+Creaous@users.noreply.github.com>
2025-04-27 16:34:22 +0930

    move files around and add cosmos audit log

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,96 +1,10 @@
-import { pgTable, primaryKey, timestamp } from "drizzle-orm/pg-core";
-import { citext } from "./types";
-import { relations, type InferSelectModel } from "drizzle-orm";
-import { post } from "./post";
-import { user } from "./user";
-
-// types MUST be first.
+// *** types MUST be first. ***
 export * from "./types";
+// ****************************
+
+export * from "./auth";
+export * from "./cosmos";
 export * from "./organization";
 export * from "./post";
+export * from "./topic";
 export * from "./user";
-export * from "./auth";
-
-// TODO: Migrate these to a separate file.
-
-export const topic = pgTable("topic", {
-  name: citext("name").notNull().primaryKey(),
-  description: citext("description"),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .defaultNow()
-    .$onUpdate(() => new Date()),
-});
-
-export const topicRelations = relations(topic, ({ many }) => ({
-  posts: many(topicPost, {
-    relationName: "topic_to_posts",
-  }),
-  users: many(topicUser, {
-    relationName: "topic_to_users",
-  }),
-}));
-
-export const topicUser = pgTable(
-  "topic_user",
-  {
-    topicName: citext("topic_name").notNull(),
-    userId: citext("user_id").notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
-      .notNull()
-      .defaultNow()
-      .$onUpdate(() => new Date()),
-  },
-  (table) => [primaryKey({ columns: [table.topicName, table.userId] })],
-);
-
-export const topicUserRelations = relations(topicUser, ({ one }) => ({
-  user: one(user, {
-    relationName: "user_topics",
-    fields: [topicUser.userId],
-    references: [user.id],
-  }),
-  topic: one(topic, {
-    relationName: "topic_to_users",
-    fields: [topicUser.topicName],
-    references: [topic.name],
-  }),
-}));
-
-export const topicPost = pgTable(
-  "topic_post",
-  {
-    topicName: citext("topic_name").notNull(),
-    postId: citext("post_id").notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
-      .notNull()
-      .defaultNow()
-      .$onUpdate(() => new Date()),
-  },
-  (table) => [primaryKey({ columns: [table.topicName, table.postId] })],
-);
-
-export const topicPostRelations = relations(topicPost, ({ one }) => ({
-  post: one(post, {
-    relationName: "post_topics",
-    fields: [topicPost.postId],
-    references: [post.id],
-  }),
-  topic: one(topic, {
-    relationName: "topic_to_posts",
-    fields: [topicPost.topicName],
-    references: [topic.name],
-  }),
-}));
-
-export type TopicSchemaType = InferSelectModel<typeof topic>;
-export type TopicUserSchemaType = InferSelectModel<typeof topicUser>;
-export type TopicPostSchemaType = InferSelectModel<typeof topicPost>;
-
-export const cosmosSetting = pgTable("cosmos_setting", {
-  key: citext("key").notNull().primaryKey(),
-  value: citext("value").notNull(),
-});
